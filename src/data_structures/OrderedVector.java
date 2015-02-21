8c603978e48ca9139c3da914ae77f8b0bed80fb0
Alicia Guerra <aiguerra@ucsd.edu>
2015-02-21 11:20:46 -0800

    Update OrderedVector.java

--- src/data_structures/OrderedVector.java
+++ src/data_structures/OrderedVector.java
@@ -16,56 +16,66 @@ public class OrderedVector<E> implements OrderedListADT<E> {
 
     public OrderedVector() {
         currentSize = 0;
-        maxSize = 1000;
+        maxSize = DEFAULT_MAX_CAPACITY;
         storage = (E[]) new Object[maxSize];
     }
-    /*1. Adds object in the list to the correct position as determined by the 
+    /*1. Adds object in the list to the correct position as determined by the
      comparable interface.*/
 
     public void insert(E obj) {
         checkSize();
-        /*checkSize after the iterator in Step 12.*/
-       storage[currentSize++]=obj;
+        int index=binSearchFindInsertionPoint(obj,0,currentSize-1);
+        for(int i=currentSize-1;i>=index;i--){
+            storage[i+1]=storage[i];
+        }
+       storage[index]=obj;
+       currentSize++;
     }
-    /*2. Removes the object located at the paramater index position (zero-based).*/
-    /*3. Throws IndexOutofBoundsException if the index does not map to a valid
+    /*2. Removes the object located at the paramater index position
+(zero-based).*/
+    /*3. Throws IndexOutofBoundsException if the index does not map to a
+valid
      position within the list.*/
 
     public E remove(int index) {
-        return shiftDelete(index);
+        if(index>currentSize-1 || index<0){
+        throw new IndexOutOfBoundsException("Index Out of Bounds");
+        }
+     E tempObject=storage[index];
+     for(int i=index;i<=currentSize-1;i++){
+         storage[i]=storage[i+1];
+     }
+     checkSize();
+     currentSize--;
+     return tempObject;
     }
-    /*4. Removes and returns the parameter object obj from the list if the list
+    /*4. Removes and returns the parameter object obj from the list if the
+list
      contains it, null otherwise.*/
 
     public E remove(E obj) {
-        int check = binSearchFindObjectIndex(obj, 0, currentSize - 1);
-        return shiftDelete(check);
+        int findThis = binSearchFindObjectIndex(obj, 0, currentSize - 1);
+        E tempObject=storage[findThis];
+        for(int i=findThis;i<=currentSize-1;i++){
+            storage[i]=storage[i+1];
+        }
+        checkSize();
+        currentSize++;
+        return tempObject;
     }
     /*5. Returns the parameter object located at the parameter index position
      (zero-based).*/
 
-    public E get(int index) {
-        E check;
-        try {
-            check = storage[index];
-        } 
-    /*6. Throws IndexOutofBoundsException if the index does not map to a valid 
-    position within the underlying array.*/ 
-        catch (IndexOutOfBoundsException E) {
-            check = null;
-        }
-        return check;
+     public E get(E obj) {
+        int findThis = binSearchFindObjectIndex(obj, 0, currentSize - 1);
+        if(findThis==-1)
+            return null;
+        return storage[findThis];
     }
-    /*7. Returns the parameter object obj if the list contains it, null otherwise.*/
 
-    public E get(E obj) {
-        int check = binSearchFindObjectIndex(obj, 0, currentSize - 1);
-        return storage[check];}
-    /*8. Returns true if the parameter object obj is in the list, false otherwise.*/
-    /*Just like the code in Riggins' SlowContainer data structure.*/
 
     public boolean contains(E obj) {
-        for (int i = 0; i < currentSize; i++) 
+        for (int i = 0; i < currentSize; i++)
             if (((Comparable<E>) obj).compareTo(storage[i]) == 0)
                 return true;
                 return false;
@@ -73,8 +83,8 @@ public class OrderedVector<E> implements OrderedListADT<E> {
     /*9. The list is returned to an empty state.*/
 
     public void clear() {
-        setMaxSize(100);
-        storage = (E[]) new Object[maxSize];
+        if(currentSize>0)
+        currentSize=0;
     }
     /*10. Returns true if the list is empty, otherwise false.*/
     /*Just like the code in Riggins' SlowContainer data structure.*/
@@ -88,12 +98,21 @@ public class OrderedVector<E> implements OrderedListADT<E> {
     public int size() {
         return currentSize;
     }
-    /*12. Returns an iterator of the values in the list, presented in the same
+    /*12. Returns an iterator of the values in the list, presented in the
+same
      order as the list.*/
     /*This part of the code was copied from Riggins' SlowContainer program.*/
 
     public Iterator<E> iterator() {
         return new IteratorHelper();
+    }
+
+    @Override
+    public E get(int index) {
+    if(index>storage.length){
+      throw new IndexOutOfBoundsException("Index Out of Bounds");
+              }
+      return storage[index];
     }
 
     class IteratorHelper implements Iterator<E> {
@@ -120,7 +139,7 @@ public class OrderedVector<E> implements OrderedListADT<E> {
         }
     }
     /*checkSize from part 1*/
-    
+
     public void checkSize() {
         if (currentSize == maxSize) {
             setMaxSize(maxSize * 2);
@@ -173,16 +192,11 @@ public class OrderedVector<E> implements OrderedListADT<E> {
         if (low > high) {
             return low;
         }
-        int mid = (low + high) / 2, comp = 1;
-        if (comp == 0) {
-            return mid;
-        } else if (comp < 0) {
-            return binSearchFindInsertionPoint(obj, low, mid - 1);
-        }
+        int mid = (low + high)>>1;
+        int comp=((Comparable<E>) obj).compareTo(storage[mid]);
+        if (comp < 0)
+        return binSearchFindInsertionPoint(obj, low, mid - 1);
         return binSearchFindInsertionPoint(obj, mid + 1, high);
     }
 }
-   
-   
-
 
